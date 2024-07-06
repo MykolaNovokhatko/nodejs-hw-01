e97e78992f30ed90b015b430457894d05dc6ef76
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-07-06 18:53:17 +0100

    hw1/2/3

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,23 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+export const addOneContact = async () => {
+  try {
+    // Читаю існуючі конт.
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    // Генерую новий конт.
+    const newContact = createFakeContact();
+    contacts.push(newContact);
+
+    // Записую оновлений масив конт.
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2));
+    console.log(newContact);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -4,13 +4,16 @@ import { createFakeContact } from '../utils/createFakeContact.js';
 
 const generateContacts = async (number) => {
   try {
+    // Читаю існуючі конт.
     const readContacts = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(readContacts);
 
+    // Генерую новий конт.
     for (let index = 0; index < number; index++) {
       contacts.push(createFakeContact());
     }
 
+    // Записую оновлений масив конт.
     await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2));
     console.log(`${number}`);
   } catch (error) {
@@ -18,4 +21,7 @@ const generateContacts = async (number) => {
   }
 };
 
+// К-сть повернутих конт.
 generateContacts(5);
+
+// npm run generate
