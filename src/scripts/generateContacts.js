a130da2669bd9e0fb6816cdac38b46c93f94f7ba
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-07-06 18:17:44 +0100

    hw1

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,21 @@
 import { PATH_DB } from '../constants/contacts.js';
+import * as fs from 'node:fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    const readContacts = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(readContacts);
+
+    for (let index = 0; index < number; index++) {
+      contacts.push(createFakeContact());
+    }
+
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, undefined, 2));
+    console.log(`${number}`);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 generateContacts(5);
